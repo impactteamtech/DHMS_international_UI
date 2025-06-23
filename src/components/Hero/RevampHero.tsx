f4cda1a979945b234da18eaa28ebfc2c83e8d5dc
Yuri561 <hazelirvin@gmail.com>
2025-06-23 06:39:27 -0400

    edited products brand

--- src/components/Hero/RevampHero.tsx
+++ src/components/Hero/RevampHero.tsx
@@ -4,35 +4,33 @@ import heroPic from '../../assets/hero.png';
 
 const RevampHero: React.FC = () => {
   return (
-    <section className="w-full bg-[#d5a86b]  px-6 py-16 md:py-24 grid grid-cols-1 md:grid-cols-2 items-center max-w-8xl mx-auto font-raleway">
+    <section className="w-full bg-[#d5a86b] px-4 sm:px-6 py-12 sm:py-16 md:py-24 grid grid-cols-1 md:grid-cols-2 items-center max-w-8xl mx-auto font-raleway">
       
       {/* Left Column - Text */}
-      <div className="flex flex-col items-center md:items-center justify-center text-center md:text-right px-4 space-y-6">
-        <div className="text-nowrap items-center justify-center pl-9 flex flex-col">
-          <h1 className="text-8xl sm:text-8xl md:text-6xl lg:text-[100px] ml-32 font-[satisfy] text-white leading-tight">
-            Where Culture Meets Beauty
-          </h1>
-          <p className="text-md sm:text-lg text-white  mt-4">
-            Unearth timeless fashion, luminous beauty, and care inspired by heritage.
-          </p>
-          <OffsetLink 
-            to="/shop#top"
-            className="inline-block mt-6 px-6 py-2 rounded-xl bg-white text-black text-sm sm:text-base font-semibold border border-white hover:scale-105 transition-transform"
-          >
-            Shop Now
-          </OffsetLink>
-        </div>
+      <div className="flex flex-col items-center md:items-start justify-center text-center mt-16 md:text-left space-y-4 sm:space-y-6 px-2 sm:px-4">
+        <h1 className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[90px] font-[satisfy] text-white leading-tight tracking-tight">
+          Where Culture Meets Beauty
+        </h1>
+        <p className="text-sm sm:text-base md:text-lg text-white max-w-md sm:max-w-lg">
+          Unearth timeless fashion, luminous beauty, and care inspired by heritage.
+        </p>
+        <OffsetLink 
+          to="/shop#top"
+          className="inline-block mt-4 sm:mt-6 px-5 sm:px-6 py-2 rounded-xl bg-white text-black text-sm sm:text-base font-semibold border border-white hover:scale-105 transition-transform"
+        >
+          Shop Now
+        </OffsetLink>
       </div>
 
       {/* Right Column - Hero Image */}
-      <div className="flex justify-center items-center mt-12 md:mt-0">
+      <div className="flex justify-center items-center mt-8 md:mt-0">
         <img 
           src={heroPic} 
-          alt="hero" 
-          className="w-64 sm:w-80 md:w-[26rem] object-cover mask-b-from-20% mask-b-to-80%"
+          alt="hero"
+          className="w-56 sm:w-72 md:w-[26rem] object-cover mask-b-from-20% mask-b-to-80%"
         />
       </div>
-      
+
     </section>
   );
 };
